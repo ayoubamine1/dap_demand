d7e2701e3c091e0c8c080e6ce60dfd5fcf398660
ayoubamine1 <96729428+ayoubamine1@users.noreply.github.com>
2023-08-01 13:25:08 +0100

    Update India_MAP_Demand.py

--- pages/India_MAP_Demand.py
+++ pages/India_MAP_Demand.py
@@ -11,12 +11,12 @@ import plotly.express as px
 
 
 
-light_filename = 'pages/Final model/lg_model.joblib'
-random_filename = 'pages/Final model/rf_model.joblib'
-mate_model_filename = 'pages/Final model/mate_model.joblib'
+light_filename = 'Final model/India Final Model/lg_model.joblib'
+random_filename = 'Final model/India Final Model/rf_model.joblib'
+mate_model_filename = 'Final model/India Final Model/mate_model.joblib'
 
-xgblags_filename = 'pages/Final model/xgboost_model_withlags.joblib'
-mata_lags_filename = 'pages/Final model/meta_model_lags.joblib'
+xgblags_filename = 'Final model/India Final Model/xgboost_model_withlags.joblib'
+mata_lags_filename = 'Final model/India Final Model/meta_model_lags.joblib'
 
 
 lgb_model = joblib.load(light_filename)
